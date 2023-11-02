810a800e954ef8b819ba1860c2bf5de00b44635e
varunnaik1121 <varunnaik1121@gmail.com>
2023-11-03 02:04:23 +0530

    added nav bar and basic auth config

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import '@Styles/globals.css';
+import Provider from '@components/Provider';
+import Nav from '@components/Nav';
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover & Share AI Prompts',
+};
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
